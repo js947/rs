0b0cbdbd38fea3c266821a2e94df7c1f5a124ef8
Jeffrey Salmond <js947@cam.ac.uk>
2020-02-29 20:03:12 +0000

    implement searching in analysis types

--- cmd/analysis.go
+++ cmd/analysis.go
@@ -3,9 +3,14 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"github.com/blevesearch/bleve"
 	"github.com/js947/rs/api"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
+	wordwrap "github.com/mitchellh/go-wordwrap"
 )
 
 func init() {
@@ -24,7 +29,7 @@ type Analysis struct {
 }
 
 func analyses(cmd *cobra.Command, args []string) {
-	addr := "https://platform.rescale.com/api/v2/analyses/?page_size=100"
+	addr := "https://platform.rescale.com/api/v2/analyses/?page_size=300"
 	var analyses []Analysis
 
 	for addr != "" {
@@ -35,15 +40,59 @@ func analyses(cmd *cobra.Command, args []string) {
 		}
 		json.Unmarshal(api.Get(addr), &capi)
 
-		fmt.Fprintf(os.Stderr, "%d, %s\n", capi.Count, capi.Next)
-
 		addr = capi.Next
 		analyses = append(analyses, capi.Results...)
 	}
 
-	f := "%12s\t%12s\t%s\n"
-	fmt.Printf(f, "code", "name", "description")
-	for _, v := range analyses {
-		fmt.Printf(f, v.Code, v.Name, v.Description)
+	if len(args) > 0 {
+		mapping := bleve.NewIndexMapping()
+
+		dir, err := ioutil.TempDir(os.TempDir(), "analyses.search")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.RemoveAll(dir)
+
+		index, err := bleve.New(dir, mapping)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, a := range analyses {
+			index.Index(a.Code, a.Description)
+		}
+
+		count, err := index.DocCount()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%d analyses indexed\n", count)
+
+		query := bleve.NewMatchQuery(strings.Join(args, " "))
+		search := bleve.NewSearchRequest(query)
+		searchResults, err := index.Search(search)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%d matches\n", len(searchResults.Hits))
+		for i, a := range searchResults.Hits {
+			var (
+				name string
+				description string
+			)
+			for _, b := range analyses {
+				if a.ID == b.Code {
+					name = b.Name
+					description = b.Description
+				}
+			}
+			fmt.Printf("%3d %20s\n", i, a.ID) 
+			fmt.Printf("%s\n%s\n\n", name, wordwrap.WrapString(description, 80)) 
+		}
+	} else {
+		for i, a := range analyses {
+			fmt.Printf("%3d %20s\n", i, a.Code) 
+			fmt.Printf("%s\n%s\n\n", a.Name, wordwrap.WrapString(a.Description, 80)) 
+		}
 	}
 }
