6df356eb82a0a7356b3693f0f08fdcecc3d3b255
Jeffrey Salmond <js947@cam.ac.uk>
2020-03-01 17:51:20 +0000

    move open to job open and implement it

---
+++ cmd/job_open.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+    "github.com/pkg/browser"
+)
+
+func init() {
+	delete := &cobra.Command{
+		Use:   "open <jobid>",
+		Short: "Open job in browser",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := job_open(cmd, args[0])
+			if err != nil {
+				panic(err)
+			}
+		},
+	}
+	jobCmd.AddCommand(delete)
+}
+
+func job_open(cmd *cobra.Command, jobid string) error {
+	platform := "https://platform.rescale.com"
+	return browser.OpenURL(fmt.Sprintf("%s/jobs/%s/", platform, jobid))
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/couchbase/vellum v0.0.0-20190829182332-ef2e028c01fd // indirect
 	github.com/etcd-io/bbolt v1.3.3 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.0
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.4.0
 	github.com/steveyen/gtreap v0.0.0-20150807155958-0abe01ef9be2 // indirect
