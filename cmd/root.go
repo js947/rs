6ee493549e1717c55878e684cf9958eec56455a7
Jeffrey Salmond <js947@cam.ac.uk>
2020-03-06 17:26:02 +0000

    print error if rescale token is missing

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -13,7 +14,7 @@ See http://github.com/js947/rs`,
 }
 
 func Execute() error {
-	rootCmd.PersistentFlags().StringP("token", "t", "", "Rescale API token")
+	rootCmd.PersistentFlags().StringP("token", "t", "unset", "Rescale API token")
 	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
 	rootCmd.PersistentFlags().String("api", "", "API address")
 	viper.BindPFlag("api", rootCmd.PersistentFlags().Lookup("api"))
@@ -29,6 +30,10 @@ func Execute() error {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			return err
 		}
+	}
+
+	if viper.GetString("token") == "unset" {
+		return fmt.Errorf("must set rescale token in ~/.rescale/config.yaml or in RESCALE_TOKEN")
 	}
 
 	return rootCmd.Execute()
